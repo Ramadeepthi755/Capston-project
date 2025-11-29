e8316dd04dbc87af0d8730825f74e800aeda7c34
Ramadeepthi <ramadeepthibadireddy@gmail.com>
2025-11-29 20:09:31 +0530

    add app.py after fixing .gitignore

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
-print("Capstone project started")
-print("Capstone Project Started Successfully!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from skin_hair_agent import diagnose
+
+app = FastAPI(
+    title="Skin & Hair Diagnosis AI",
+    description="Simple AI-based API to analyze skin and hair problems",
+    version="1.0"
+)
+
+class RequestIn(BaseModel):
+    topic: str
+    symptom: str
+
+@app.get("/")
+def home():
+    return {"message": "API is Running Successfully!"}
+
+@app.post("/analyze")
+def analyze(req: RequestIn):
+    print(">> /analyze called with:", req.dict())
+    result = diagnose(req.topic, req.symptom)
+    print(">> result:", result)
+    return result
+
